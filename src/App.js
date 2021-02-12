653841e9179e4830b61d0079da70f3b2d2af125d
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-02-11 20:07:20 -0500

    project front end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
+import React, { Component } from 'react'
+import axios from 'axios'
 
-function App() {
-    state = {
+
+class App extends React.Component {
+    state =  {
         body: "",
-        votes: 0,
-
+        vote: 0
     }
 
-    
+    render = () => {
+        return (
+            <div className="App">
+                <div>
+                <p>Welcome to happiness where you can escape and be in your happy place.
 
-    return (
-        <div className="App">
-            <div>
-            <p>Welcome to happiness where you can escape and be in your happy place.
+                This is what makes me happy</p>
 
-            This is what makes me happy</p>
-
-            <button>Pictures</button>
-            <img src="https://cdn.discordapp.com/attachments/618539506529992705/706640070253346826/lilyosqwikw41.png" alt=""></img>
-            <button>Music</button>
+                <button className="picture_button">Pictures</button>
+                <img clasName="cat" src="https://cdn.discordapp.com/attachments/618539506529992705/706640070253346826/lilyosqwikw41.png" alt=""></img>
+                <button>Music</button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
 
 export default App;
