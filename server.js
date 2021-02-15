8a5606d449cf52b5aa209eed65d4df0d1b97cfb1
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-02-15 09:43:51 -0500

    testing heroku push

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const express = require('express')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const app = express()
 const db = mongoose.connection
 require('dotenv').config()
-dotenv.config()
-const app = express()
-const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
 
+const PORT = process.env.PORT || 3003
+
+const MONGODB_URI = process.env.MONGODB_URI
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
@@ -17,25 +17,19 @@ const commentsController = require('./controllers/comments_controller.js')
 app.use('/comments', commentsController)
 
 
-//Database Connection
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
+mongoose.connect(
+    MONGODB_URI,
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 })
-// DB connection test
-mongoose.connection.on('error', err =>
-  console.log(
-    err.message,
-    ' is Mongod not running?/Problem with Atlas Connection?'
-  )
-)
-mongoose.connection.on('connected', () =>
-  console.log('mongo connected: ', MONGODB_URI)
-)
-mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
 
-// Listening
-app.listen(PORT, ()=>{
-    console.log('listening on port: ', PORT)
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'))
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
+
+app.listen(PORT, () => {
+  console.log('Listening on port: ', PORT)
 })
